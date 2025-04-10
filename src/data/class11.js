7f4ce422c6bc34234d9100718215aa03d089c46f
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-10 13:35:33 +0530

    Create class11.js

---
+++ src/data/class11.js
@@ -0,0 +1,74 @@
+const class11 = [
+  {
+    subject: "Physics",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Chemistry",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Biology",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Maths",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "English",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default class11;
