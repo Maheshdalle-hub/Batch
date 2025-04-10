c056ce19e67594560a77b37ae7d98916a2053521
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-10 13:34:12 +0530

    Create class10.js

---
+++ src/data/class10.js
@@ -0,0 +1,102 @@
+const class10 = [
+  {
+    subject: "Maths",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+            notesUrl: "pdf",
+          },
+          {
+            lectureName: "Lecture 2",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+      {
+        chapterName: "Chapter 2",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Science",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "SST",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Hindi",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "English",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "IT",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default class10;
