cf25d97986897315dad5a2a79f94007fa14fec9a
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-10 13:07:02 +0530

    Create class9.js

---
+++ src/data/class9.js
@@ -0,0 +1,47 @@
+const class9 = [
+  {
+    subject: "Maths", 
+    chapters: [ 
+      {
+        chapterName: "Chapter 1",
+        lectures: [ 
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "m3u8linkhere",
+            notesUrl: "linkhere",
+          },
+          {
+            lectureName: "Lecture 2",
+            m3u8Url: "https://example.com/class9/maths/ch1/lec2.m3u8",
+          },
+        ],
+      },
+      {
+        chapterName: "Chapter 2",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "https://example.com/class9/maths/ch2/lec1.m3u8",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    subject: "Science",
+    chapters: [
+      {
+        chapterName: "Chapter 1",
+        lectures: [
+          {
+            lectureName: "Lecture 1",
+            m3u8Url: "https://example.com/class9/science/ch1/lec1.m3u8",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+
+export default class9;
