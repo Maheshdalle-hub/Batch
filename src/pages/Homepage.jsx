127e15fe3ecb6db90919a56c49ad4fd123ebd6f2
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-10 18:14:25 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,26 +7,25 @@ import mlogo from "../assets/ntmlogo.jpg";
 const Homepage = () => {
   const navigate = useNavigate();
 
-  const handleClick = (grade) => {
-    // Store the redirect path with the correct grade
-    sessionStorage.setItem("redirectAfterLogin", `/subjects/${grade}`);
-    navigate("/login"); // Navigate to the login page
+  const handleClick = (path) => {
+    localStorage.setItem("redirectAfterLogin", path);  // Store the redirect path in localStorage
+    navigate("/login");  // Redirect to login page
   };
 
   return (
     <div className="container">
       <img src={mlogo} alt="Logo" className="big-logo" />
 
-      <div className="click-box" onClick={() => handleClick(10)}>
+      <div className="click-box" onClick={() => handleClick("/subjects/10")}>
         <img src={imageUrl} alt="Aarambh Batch 2025-26" className="homepage-image" />
         <h1>Aarambh Batch 2025-26</h1>
       </div>
 
-      <div className="click-box" onClick={() => handleClick(9)}>
+      <div className="click-box" onClick={() => handleClick("/subjects/9")}>
         <h1>Class 9</h1>
       </div>
 
-      <div className="click-box" onClick={() => handleClick(11)}>
+      <div className="click-box" onClick={() => handleClick("/subjects/11")}>
         <h1>Class 11</h1>
       </div>
     </div>
