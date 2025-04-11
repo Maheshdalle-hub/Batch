3eee2760fc0534965a417a27e4dac359beacc698
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-11 11:50:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
-import Subject9 from "./pages/Subject9";     // Class 9 subjects
-import Subject10 from "./pages/Subject10";   // Class 10 subjects
-import Subject11 from "./pages/Subject11";   // Class 11 subjects
-import Lectures9 from "./pages/Lectures9";
-import Lectures from "./pages/Lectures";     // Class 10
-import Lectures11 from "./pages/Lectures11";
-import ChapterLectures from "./pages/ChapterLectures";
 import Verify from "./pages/Verify";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Subject9 from "./pages/Subject9";
+import Subject10 from "./pages/Subject10";
+import Subject11 from "./pages/Subject11";
+import Lectures9 from "./pages/Lectures9";
+import Lectures from "./pages/Lectures";
+import Lectures11 from "./pages/Lectures11";
+import ChapterLectures9 from "./pages/ChapterLectures9";
+import ChapterLectures10 from "./pages/ChapterLectures10";
+import ChapterLectures11 from "./pages/ChapterLectures11";
+import ProtectedRoute from "./ProtectedRoute";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/verify/:token" element={<Verify />} />
 
-        {/* Subjects */}
-        <Route
-          path="/subjects/9"
-          element={<ProtectedRoute><Subject9 /></ProtectedRoute>}
-        />
-        <Route
-          path="/subjects/10"
-          element={<ProtectedRoute><Subject10 /></ProtectedRoute>}
-        />
-        <Route
-          path="/subjects/11"
-          element={<ProtectedRoute><Subject11 /></ProtectedRoute>}
-        />
+        {/* Subject Pages */}
+        <Route path="/subjects/9" element={<ProtectedRoute><Subject9 /></ProtectedRoute>} />
+        <Route path="/subjects/10" element={<ProtectedRoute><Subject10 /></ProtectedRoute>} />
+        <Route path="/subjects/11" element={<ProtectedRoute><Subject11 /></ProtectedRoute>} />
 
-        {/* Lectures */}
-        <Route
-          path="/lectures/9/:subject"
-          element={<ProtectedRoute><Lectures9 /></ProtectedRoute>}
-        />
-        <Route
-          path="/lectures/:subject"  // Class 10
-          element={<ProtectedRoute><Lectures /></ProtectedRoute>}
-        />
-        <Route
-          path="/lectures/11/:subject"
-          element={<ProtectedRoute><Lectures11 /></ProtectedRoute>}
-        />
+        {/* Lecture Pages */}
+        <Route path="/lectures/9/:subject" element={<ProtectedRoute><Lectures9 /></ProtectedRoute>} />
+        <Route path="/lectures/10/:subject" element={<ProtectedRoute><Lectures /></ProtectedRoute>} />
+        <Route path="/lectures/11/:subject" element={<ProtectedRoute><Lectures11 /></ProtectedRoute>} />
 
         {/* Chapter Lectures */}
-        <Route
-          path="/chapter-lectures/:classId/:subject/:chapterIndex"
-          element={<ProtectedRoute><ChapterLectures /></ProtectedRoute>}
-        />
+        <Route path="/chapter-lectures/9/:subject/:chapterIndex" element={<ProtectedRoute><ChapterLectures9 /></ProtectedRoute>} />
+        <Route path="/chapter-lectures/10/:subject/:chapterIndex" element={<ProtectedRoute><ChapterLectures10 /></ProtectedRoute>} />
+        <Route path="/chapter-lectures/11/:subject/:chapterIndex" element={<ProtectedRoute><ChapterLectures11 /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
