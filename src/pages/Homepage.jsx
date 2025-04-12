d1a1ec9e55fe7f17d09083baa02c7ca36795e729
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-12 13:36:07 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
-import imageUrl from "./IMG_20250227_100103_636.jpg";
+import imageUrl10 from "../assets/10img.jpg"; // 10th class image
+import imageUrl9 from "../assets/9img.png"; // Class 9 image
+import imageUrl11 from "../assets/11sci.jpg"; // Class 11 image
 import mlogo from "../assets/ntmlogo.jpg";
 
 const Homepage = () => {
@@ -26,15 +28,17 @@ const Homepage = () => {
       <img src={mlogo} alt="Logo" className="big-logo" />
 
       <div className="click-box" onClick={() => handleClick(10)}>
-        <img src={imageUrl} alt="Aarambh Batch 2025-26" className="homepage-image" />
+        <img src={imageUrl10} alt="Aarambh Batch 2025-26" className="homepage-image" />
         <h1>Aarambh Batch 2025-26</h1>
       </div>
 
       <div className="click-box" onClick={() => handleClick(9)}>
+        <img src={imageUrl9} alt="Class 9 Batch" className="homepage-image" />
         <h1>Class 9</h1>
       </div>
 
       <div className="click-box" onClick={() => handleClick(11)}>
+        <img src={imageUrl11} alt="Class 11 Batch" className="homepage-image" />
         <h1>Class 11</h1>
       </div>
     </div>
