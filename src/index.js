6a3fa6bb05fd238a511333ac53dc6b30fac4593a
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-12 12:37:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use `react-dom/client` for React 18
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./styles/global.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Create root using React 18
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
