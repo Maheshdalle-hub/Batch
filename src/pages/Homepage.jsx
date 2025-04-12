abaf65d76f003e571336efca342a12fcacb04f6b
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-12 21:40:26 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -25,23 +25,26 @@ const Homepage = () => {
 
   return (
     <div className="container">
-      <img src={mlogo} alt="Logo" className="big-logo" />
+  <img src={mlogo} alt="Logo" className="big-logo" />
+  <h2 className="section-heading">Our Batches</h2>
 
-      <div className="click-box" onClick={() => handleClick(10)}>
-        <img src={imageUrl10} alt="Aarambh Batch 2025-26" className="homepage-image" />
-        <h1>Aarambh Batch 2025-26</h1>
-      </div>
-
-      <div className="click-box" onClick={() => handleClick(9)}>
-        <img src={imageUrl9} alt="Class 9 Batch" className="homepage-image" />
-        <h1>Class 9</h1>
-      </div>
-
-      <div className="click-box" onClick={() => handleClick(11)}>
-        <img src={imageUrl11} alt="Class 11 Batch" className="homepage-image" />
-        <h1>Class 11</h1>
-      </div>
+  <div className="batch-container">
+    <div className="click-box" onClick={() => handleClick(10)}>
+      <img src={imageUrl10} alt="Aarambh Batch 2025-26" className="homepage-image" />
+      <h1>Aarambh Batch 2025-26</h1>
     </div>
+
+    <div className="click-box" onClick={() => handleClick(9)}>
+      <img src={imageUrl9} alt="Class 9 Batch" className="homepage-image" />
+      <h1>Class 9</h1>
+    </div>
+
+    <div className="click-box" onClick={() => handleClick(11)}>
+      <img src={imageUrl11} alt="Class 11 Batch" className="homepage-image" />
+      <h1>Class 11</h1>
+    </div>
+  </div>
+</div>
   );
 };
 
