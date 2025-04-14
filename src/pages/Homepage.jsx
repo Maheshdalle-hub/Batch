e59bfa3bbfe2585ebcdaed583d5f9cea57227a31
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-15 00:01:06 +0530

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -24,7 +24,6 @@ const Homepage = () => {
   };
 
   return (
-    <h1 className="emp">EduVibe-Nt</h1>
     <div className="container">
   <img src={mlogo} alt="Logo" className="big-logo" />
   <h2 className="section-heading">Our Batches</h2>
