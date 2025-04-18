d9dab88e5f94cbec58044f1f984d78cb3dd69a79
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-19 00:13:58 +0530

    Update ChapterLectures11.jsx

--- src/pages/ChapterLectures11.jsx
+++ src/pages/ChapterLectures11.jsx
@@ -17,7 +17,7 @@ const ChapterLectures11 = () => {
       0: [
         {
           name: "Lecture 1",
-          youtubeUrl: "https://www.youtube.com/live/RF6xfHVo9xc", // example
+          youtubeUrl: "https://www.youtube.com/live/RF6xfHVo9xc",
           notesUrl: "",
         },
         {
@@ -51,12 +51,12 @@ const ChapterLectures11 = () => {
           m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4354032/174490568189301097666/index_2.m3u8",
           notesUrl: "https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/320554157609896960.pdf"
         },
-       {
-         name: "Lecture 3",
-         m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4354885/174499396879388296383/index_2.m3u8",
-         notesUrl: "https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/282566967198221150.pdf"
-       }
-     ],
+        {
+          name: "Lecture 3",
+          m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4354885/174499396879388296383/index_2.m3u8",
+          notesUrl: "https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/282566967198221150.pdf"
+        }
+      ],
       19: [
         {
           name: "PCP 01",
@@ -77,56 +77,61 @@ const ChapterLectures11 = () => {
         },
       ],
     },
-
   };
 
   const handleLectureClick = (lecture) => {
     if (lecture.youtubeUrl) {
       window.location.href = lecture.youtubeUrl;
     }
-  }
-  if (lecture.redirect) {
-            return (
-              <div
-                key={index}
-                onClick={() => window.location.href = lecture.redirect}
-                className="lecture-box"
-              >
-                {lecture.name}
-              </div>
-            );
-  }
+  };
 
   return (
     <div className="chapter-lectures-container">
       <img src={mlogo} alt="Logo" className="big-logo" />
       <h2>{subject} - Chapter {parseInt(chapterIndex) + 1}</h2>
       <div className="lecture-boxes">
-        {chapterLectures[subject]?.[chapterIndex]?.map((lecture, index) => (
-          lecture.youtubeUrl ? (
-            <div
-              key={index}
-              className="lecture-box"
-              onClick={() => handleLectureClick(lecture)}
-              style={{ cursor: "pointer" }}
-            >
-              {lecture.name} (YouTube)
-            </div>
-          ) : (
-            <Link
-              key={index}
-              to={`/video/11/${subject}/${chapterIndex}`}
-              state={{
-                chapterName: lecture.name,
-                m3u8Url: lecture.m3u8Url,
-                notesUrl: lecture.notesUrl,
-              }}
-              className="lecture-box"
-            >
-              {lecture.name}
-            </Link>
-          )
-        ))}
+        {chapterLectures[subject]?.[chapterIndex]?.map((lecture, index) => {
+          if (lecture.redirect) {
+            return (
+              <div
+                key={index}
+                onClick={() => window.location.href = lecture.redirect}
+                className="lecture-box"
+                style={{ cursor: "pointer" }}
+              >
+                {lecture.name} (PDF)
+              </div>
+            );
+          } else if (lecture.youtubeUrl) {
+            return (
+              <div
+                key={index}
+                onClick={() => handleLectureClick(lecture)}
+                className="lecture-box"
+                style={{ cursor: "pointer" }}
+              >
+                {lecture.name} (YouTube)
+              </div>
+            );
+          } else if (lecture.m3u8Url) {
+            return (
+              <Link
+                key={index}
+                to={`/video/11/${subject}/${chapterIndex}`}
+                state={{
+                  chapterName: lecture.name,
+                  m3u8Url: lecture.m3u8Url,
+                  notesUrl: lecture.notesUrl,
+                }}
+                className="lecture-box"
+              >
+                {lecture.name}
+              </Link>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
     </div>
   );
