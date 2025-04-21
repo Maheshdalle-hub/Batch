73df40406fb0bc4d707a1650e06095d0d539001b
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-21 16:11:26 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUPU5jgSka89Io_y9FhKc-0RlhfSujFXU",
+  authDomain: "studyapp-c37eb.firebaseapp.com",
+  projectId: "studyapp-c37eb",
+  storageBucket: "studyapp-c37eb.firebasestorage.app",
+  messagingSenderId: "22291109354",
+  appId: "1:22291109354:web:9a89ddb19bc7e61fed0e93",
+  measurementId: "G-EGD6MEJ3ZC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
