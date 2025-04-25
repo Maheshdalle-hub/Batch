548c87b927d9fb1eb5cabd46185e393b4b536d31
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-25 20:29:05 +0530

    Update ChapterLectures9.jsx

--- src/pages/ChapterLectures9.jsx
+++ src/pages/ChapterLectures9.jsx
@@ -15,7 +15,21 @@ const ChapterLectures9 = () => {
   const chapterLectures = {
     Science: {
       0: [
-        { name: "Lecture 1", m3u8Url: "YOUR_M3U8_LINK", notesUrl: "" },
+        { 
+         name: "Lecture 1",
+         m3u8Url: "YOUR_M3U8_LINK",
+         notesUrl: "" 
+        },
+        {
+      name: "Lecture 2",
+      m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4357724/174532620129898296383/174532620129898296383_8296383.m3u8",
+      notesUrl: ""
+      },
+        {
+          name: "Lecture 3",
+          notesUrl: "",
+          m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4359011/174542283188648296383/174542283188648296383_8296383.m3u8"
+        },
       ],
     },
     // Add more subjects here...
