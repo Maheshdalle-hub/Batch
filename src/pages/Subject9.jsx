690d8f1c8d0ae7668dfc9a3c1200de446ff56358
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-29 20:07:49 +0530

    Update Subject9.jsx

--- src/pages/Subject9.jsx
+++ src/pages/Subject9.jsx
@@ -13,7 +13,7 @@ const Subject9 = () => {
 
   const schedule = {
     Monday: { "17:00": "Science", "20:00": "SST" },
-    Tuesday: { "17:00": "Science", "19:00": "Maths" },
+    Tuesday: { "17:00": "Science", "20:00": "SST" },
     Wednesday: { "17:00": "SST", "20:00": "Science" },
     Thursday: { "17:00": "SST", "20:00": "Maths" },
     Friday: { "17:00": "Maths" },
