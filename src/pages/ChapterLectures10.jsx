16d46c9a6ef6ec6e778ba6a6076c67ffb83a9db0
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-04-30 22:05:06 +0530

    Update ChapterLectures10.jsx

--- src/pages/ChapterLectures10.jsx
+++ src/pages/ChapterLectures10.jsx
@@ -225,6 +225,11 @@ const ChapterLectures10 = () => {
           notesUrl: "",
           m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4364563/174593072250617880624/174593072250617880624_7880624.m3u8"
         },
+        {
+          name: "L 6",
+          noresUrl: "",
+          m3u8Url: "https://d1qcficr3lu37x.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4366034/174602805446617880624/174602805446617880624_7880624.m3u8"
+        },
       ],
       19: [
         {
