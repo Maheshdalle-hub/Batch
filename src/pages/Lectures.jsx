e63285e4e82d6b1f91b4e9425575be7a580dda66
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-05-11 13:05:34 +0530

    Update Lectures.jsx

--- src/pages/Lectures.jsx
+++ src/pages/Lectures.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import "../styles/Lectures.css"; 
 
