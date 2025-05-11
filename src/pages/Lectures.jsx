c5bca6b112b23de577516fe0514b2cbceaf409ac
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-05-11 13:02:23 +0530

    Update Lectures.jsx

--- src/pages/Lectures.jsx
+++ src/pages/Lectures.jsx
@@ -5,6 +5,7 @@ import "../styles/Lectures.css";
 const Lectures = () => {
   const { subject } = useParams();
   const navigate = useNavigate();
+  const [selectedTab, setSelectedTab] = useState("CourseA");
 
   // ✅ Redirect if user is not logged in
   useEffect(() => {
@@ -39,11 +40,16 @@ const Lectures = () => {
       { name: "Chapter 1", index: 0 },
       { name: "Chapter 2", index: 1 },
     ],
-    English: [
+    English: {
+      CourseA:[
       { name: "A Letter to God", index: 0 },
       { name: "A Dust of Snow", index: 1},
       { name: "Grammer", index: 19 },
-    ],
+        ],
+      CourseB:[
+        { name: "Chapter 1", index:0 },
+      ],
+     },
     Hindi: [
       { name: "Chapter 1", index: 0 },
     ],
@@ -52,10 +58,31 @@ const Lectures = () => {
     ],
   };
 
+  const isTabSubject = subject === "English" || subject === "Hindi";
+  const subjectLectures = lectures[subject];
+
   return (
     <div className="lectures-container">
       <img src="https://dxixtlyravvxx.cloudfront.net/540/admin_v1/sample/49501700_Next%20toppers%20Slider%202025-2.png" alt="Weekly Planner" className="tt" />
       <h2>{subject} Lectures</h2>
+
+       {isTabSubject && (
+        <div className="tabs">
+          <button
+            className={`tab-button ${selectedTab === "CourseA" ? "active" : ""}`}
+            onClick={() => setSelectedTab("CourseA")}
+          >
+            Course A
+          </button>
+          <button
+            className={`tab-button ${selectedTab === "CourseB" ? "active" : ""}`}
+            onClick={() => setSelectedTab("CourseB")}
+          >
+            Course B
+          </button>
+        </div>
+      )}
+      
       <div className="lecture-boxes">
         {lectures[subject]?.map((lecture, index) => (
           <Link
