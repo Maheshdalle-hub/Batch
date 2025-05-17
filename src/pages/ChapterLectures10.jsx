b0879798583bfa915728bfe1b140f4699d0968bd
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-05-17 14:36:36 +0530

    Update ChapterLectures10.jsx

--- src/pages/ChapterLectures10.jsx
+++ src/pages/ChapterLectures10.jsx
@@ -262,6 +262,10 @@ const ChapterLectures10 = () => {
           name: "DPP 12",
           redirect: "https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/793376952012110200.pdf"
         },
+        {
+          name: "DPP 13",
+          redirect: "https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/295999082180152200.pdf"
+        },
       ],
     },
     SST: {
