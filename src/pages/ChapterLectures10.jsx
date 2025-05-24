c97cbbef4e6bd12d2d48ed95764c8311907affaf
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-05-24 16:39:20 +0530

    Update ChapterLectures10.jsx

--- src/pages/ChapterLectures10.jsx
+++ src/pages/ChapterLectures10.jsx
@@ -258,7 +258,7 @@ const ChapterLectures10 = () => {
         },
         {
           name: "L 3",
-          m3u8Url: "https://m3u8-player-ten.vercel.app/player?baseUrl=https://d1kw75zcv4u98c.cloudfront.net/out/v1/287810d967cc428e9bd992002e55b72c/&startNum=32900&endNum=33242&queryParam=?m=1733765650",
+          redirect: "https://m3u8-player-ten.vercel.app/player?baseUrl=https://d1kw75zcv4u98c.cloudfront.net/out/v1/287810d967cc428e9bd992002e55b72c/&startNum=32900&endNum=33421&queryParam=?m=1733765650",
           notesUrl: ""
         },
       ],
