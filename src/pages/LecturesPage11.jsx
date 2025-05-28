696f18f8a8250424799ff5f8eb0719ae32101e97
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-05-28 15:03:59 +0530

    Update LecturesPage11.jsx

--- src/pages/LecturesPage11.jsx
+++ src/pages/LecturesPage11.jsx
@@ -127,7 +127,7 @@ const LecturesPage11 = () => {
               {lectures.map((lecture, index) => (
                 <div
                   key={index}
-                  onClick={() => goToVideo(index)}
+                  onClick={() => goToVideo(index), localStorage.setItem("chapterName", lectures[index].name) }
                   style={{
                     marginBottom: 12,
                     padding: 15,
