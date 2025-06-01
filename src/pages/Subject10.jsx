080b9c2169fa013e4c0c06a72bb0a8f18d25fd9c
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-06-01 14:07:01 +0530

    Update Subject10.jsx

--- src/pages/Subject10.jsx
+++ src/pages/Subject10.jsx
@@ -19,7 +19,7 @@ const Subject10 = () => {
     Thursday: { "17:00": "Science", "20:00": "SST" },
     Friday: { "17:00": "Science", "20:00": "Maths" },
     Saturday: { "17:00": "Science", "20:00": "Maths" },
-    Sunday: { "17:00": "Maths" },
+    Sunday: { "17:00": "Science" },
   };
 
   const subjectLinks = {
