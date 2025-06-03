5b8f68cb25078b49a915b45016fee809f83b6b38
Maheshdalle-hub <dallemahesh99@gmail.com>
2025-06-03 13:02:41 +0530

    Create testData.js

---
+++ src/pages/testData.js
@@ -0,0 +1,275 @@
+const testData = {
+  Maths: {
+    "Test 1": [
+      {
+        question: "The exponent of 2 in the prime factorization of 144, is",
+        options: ["4", "5", "6", "3"],
+        correctAnswer: 1,
+        explanation: "Prime Factorisation karne ke baad 2 jitni baar repeat ho rha hai wo"
+      },
+      {
+        question: "If two positive integers a and b are expressible in the form a = pq² and b = p³q; p, q being prime numbers, then LCM (a, b) is:",
+        options: ["pq", "p³q³", "p³q²", "p²q²"],
+        correctAnswer: 3,
+        explanation: "LCM bro/sis .."
+      },
+      {
+        question: "If the sum of LCM and HCF of two numbers is 1260 and their LCM is 900 more than their HCF, then the product of two numbers is:",
+        options: ["203400", "194400", "198400", "205400"],
+        correctAnswer: 2,
+        explanation: "Hmm Equations in Two Variables"
+      },
+      {
+        question: "Two tankers contain 850 litres and 680 litres of petrol respectively. Find the maximum capacity of a container which can measure the petrol of either tanker in exact number of times:",
+        options: ["245", "190", "170", "125"],
+        correctAnswer: 3,
+        explanation: "HCF"
+      },
+      {
+        question: "For what value of k, is 3 a zero of polynomial 2x²+x+k?:",
+        options: ["40", "21", "19", "26"],
+        correctAnswer: 2,
+        explanation: "Hmm"
+      },
+      {
+        question: "In a morning walk three persons step-off together, their steps measure 80 cm ,85 cm and 90 cm respectively. What is the minimum distance each should walk so that he can cover the distance in complete steps?:",
+        options: ["26357 cm", "12240 cm", "18379 cm", "19023 cm"],
+        correctAnswer: 2,
+        explanation: "LCM"
+      },
+      {
+        question: "The HCF of 45 and 105 is 15. What is their LCM:",
+        options: ["315", "210", "310", "197"],
+        correctAnswer: 1,
+        explanation: "Hmm"
+      },
+    ],
+  },
+  Science: {
+    "Test 1": [
+      {
+        question: "In electrolysis of water, the volume of gas collected at cathode is:",
+        options: ["Half of that at anode", "Double of that at anode", "Same as anode", "None"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "What are the gases collected at cathode and anode during electrolysis of water?",
+        options: ["Hydrogen at anode, Oxygen at cathode", "Oxygen at anode, Hydrogen at cathode", "Nitrogen at anode, Oxygen at cathode", "Hydrogen at both electrodes"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "A colourless gas ‘A’ with a pungent smell is released when a pale green compound ‘B’ is heated. ‘A’ turns moist red litmus paper blue.  Identify A and B.",
+        options: ["A = Cl₂, B = CuCO₃", "A = NH₃, B = (NH₄)₂CO₃", "A = SO₂, B = FeSO₄", "A = H₂S, B = PbS"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Reaction of CaO with H₂O is an example of:",
+        options: ["Redox reaction", "Displacement reaction", "Combination and Exothermic reaction", "Decomposition reaction"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Silver chloride when exposed to sunlight turns:",
+        options: ["Green", "Grey", "Brown", "Yellow"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Heating of lead nitrate produces:",
+        options: ["Brown fumes and yellow residue", "Brown fumes and white residue", "White fumes and black residue", "Yellow gas and brown residue"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: "A metal X when heated in air forms an oxide Y. Y reacts with hydrochloric acid to give a salt and water. X and Y are:",
+        options: ["Cu and CuO", "Na and Na₂O", "Fe and Fe₂O₃", "Zn and ZnO"],
+        correctAnswer: 4,
+        explanation: ""
+      },
+      {
+        question: "A compound 'X' is used in whitewashing. It reacts with water to form 'Y'. Identify X and Y:",
+        options: ["X = CaO, Y = Ca(OH)₂", "X = Na₂CO₃, Y = NaOH", "X = Ca(OH)₂, Y = CaCO₃", "X = CaCO₃, Y = CaO"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: "In the reaction MnO₂ + 4HCl → MnCl₂ + Cl₂ + 2H₂O, the reducing agent is:",
+        options: ["HCl₂", "MnO₂", "MnCl₂", "H₂O"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: "Which of the following is both an oxidation and reduction reaction?",
+        options: ["CuSO₄ → ZnSO₄ + Cu", "Zn + CuSO₄ → ZnSO₄ + Cu", "AgNO₃+ NaCl → AgCl + NaNO₃", "NaOH + HCI → NaCl + H₂O"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Respiration is:",
+        options: ["Endothermic process", "Exothermic process", "Photochemical process", "Decomposition process"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "When magnesium ribbon burns in oxygen, the product formed is:",
+        options: ["Magnesium oxide", "Magnesium nitrate", "Magnesium carbonate", "Magnesium chloride"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: "A blue-coloured compound 'A' is heated strongly in a test tube. A black solid 'B' and a colourless gas 'C' are formed. The gas turns lime water milky.  Identify A, B, and C.",
+        options: ["A = CuSO₄·5H₂O, B = CuO, C = CO₂", "A = CuCO₃, B = CuO, C = CO₂", "A = Cu(NO₃)₂, B = Cu, C = NO₂", "A = ZnCO₃, B = Zn, C = CO"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "A silvery metal 'A' reacts with a blue solution 'B' to give a colourless solution 'C' and reddish-brown metal 'D'. Identify A, B, C, and D.",
+        options: ["A = Zn, B = CuSO, C = ZnSO₄, D = Cu", "A = Fe, B = AgNO₃, C = Fe(NO₂), and D = Ag", "A = Al, B = FeSO₄, C = Al₂(SO₄)₃, D = Fe", "A = Mg, B = ZnSO₄, C = MgSO₄, D = Zn"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+    ],
+  },
+  SST: {
+    "Test 1": [
+      {
+        question: "Different persons could have ________ notions of a country’s development.",
+        options: ["Different as well as conflicting", "Same", "Indifferent", "None of the above"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: `Match the individuals with their most likely development goals:
+| Person| Development Goal
+| A. A rural landless labourer | 1.Better job opportunities in cities
+| B. An industrialist| 2.Higher profits and business growth
+| C. A girl from a village| 3.Freedom and access to education
+| D. An urban unemployed youth | 4. More days of work and better wages`,
+        options: ["A-4, B-2, C-3, D-1", "A-1, B-2, C-4, D-3", "A-2, B-4, C-1, D-3", "A-3, B-1, C-2, D-4"],
+        correctAnswer: 1,
+        explanation: "",
+        usePre: true
+      },
+      {
+        question: "Which one of the following is the correct meaning of ‘Average Income’?",
+        options: ["The total income of the country divided by its total population.", "The total income of the country divided by its earning population.", "The total income of all the residents of the country.", "The total income from the domestic and foreign sources."],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Which of the following is an example of sustainable development?",
+        options: ["Cutting forests for factories", "Using solar energy", "Mining without control", "Overusing groundwater"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "According to the World Bank, a country with per capita income of US$ 2400 or less in 2023 is considered:",
+        options: ["Middle-income country", "Developed country", "Low-income country", "Rich country"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Assertion (A): Per capita income is not the only criterion for measuring development. Reason (R): Other factors like education, health, and public facilities also play an important role.",
+        options: ["Both A and R are true, and R is the correct explanation of A", "Both A and R are true, but R is not the correct explanation of A", "A is true, R is false", "A is false, R is true"],
+        correctAnswer: 1,
+        explanation: ""
+      },
+      {
+        question: "Assertion (A): Sustainable development ensures that resources are used carefully. Reason (R): Non-renewable resources like coal and petroleum are unlimited.",
+        options: ["Both A and R are true, and R is the correct explanation of A", "Both A and R are true, but R is not the correct explanation of A", "A is true, R is false", "A is false, R is true"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "What does a low Infant Mortality Rate (IMR) indicate?",
+        options: ["Poor healthcare services", "High number of child deaths", "Better healthcare and nutrition for infants", "High population growth"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Assume there are four families in a country. The average per capita income of these families is, ₹10000. If the income of three families is ₹8000, ₹14000 and ₹6000 respectively, what is the income of a fourth family?",
+        options: ["₹15,000", "₹6,000", "₹4,000", "₹12,000"],
+        correctAnswer: 4,
+        explanation: ""
+      },
+      {
+        question: "Which of the following states has the highest literacy rate according to 2018 study reports?",
+        options: ["Haryana", "Bihar", "Punjab", "Kerela"],
+        correctAnswer: 4,
+        explanation: "Kerela, sir, 100% literacy sir"
+      },
+      {
+        question: "In which of the following states has mining caused severe land degradation?",
+        options: ["Kerala", "Jharkhand", "Punjab", "Haryana"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: "Which method is most suitable for preventing soil erosion on hill slopes?",
+        options: ["Strip cropping", "Terrace farming", "Afforestation", "Contour ploughing"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+      {
+        question: `Match the following soil types with their key characteristics:
+
+Soil Feature   
+A. Black soil            1. Found in high rainfall areas, low humus
+B. Red soil               2. Formed from igneous rocks, retains moisture
+C. Laterite soil          3. Found in low rainfall areas, poor in nitrogen
+ `,
+        options: ["A-2, B-3, C-1", "A-1, B-2, C-3", "A-3, B-1, C-2", "A-2, B-1, C-3"],
+        correctAnswer: 1,
+        explanation: "",
+        usePre: true
+      },
+      {
+        question: "What is the classification for land that has been degraded and is unable to support crop cultivation for more than five years?",
+        options: ["Wasteland", "Arid land", "Barren land", "Forest land"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Which of the following best explains why resource planning is essential in India?",
+        options: ["Resources are plenty in all regions", "All resources are renewable", "Resources are evenly distributed", "Resources are unevenly distributed and overused"],
+        correctAnswer: 4,
+        explanation: ""
+      },
+      {
+        question: "Identify the incorrect statement:",
+        options: ["Net Sown Area refers to the land sown only once in a year", "In India, net sown area is unevenly distributed among states", "Net Sown Area helps in understanding agricultural intensity", "States with high population density always have high net sown area"],
+        correctAnswer: 4,
+        explanation: ""
+      },
+      {
+        question: "Which of the following is a major cause of land degradation in states like Gujarat, Rajasthan, and Madhya Pradesh?",
+        options: ["Deforestation", "Mining", "Overgrazing", "Industrial waste"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Ploughing along the contour lines to decelerate the flow of water down the slopes is called:",
+        options: ["Strip cropping", "Sheet erosion", "Contour ploughing", "Terrace cultivation"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "What percentage of India’s land is plain?",
+        options: ["30%", "27%", "43%", "50%"],
+        correctAnswer: 3,
+        explanation: ""
+      },
+      {
+        question: "Which of the following soil are more common in piedmont plains such as Duars, Chos and Terai?",
+        options: ["Black soil", "Alluvial soil", "Laterite soil", "None of these"],
+        correctAnswer: 2,
+        explanation: ""
+      },
+    ],
+  },
+};
+
+export default testData;
